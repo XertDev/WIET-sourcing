a67ed48f7221ef5be08c25f9d5621cc1eac618e4
Mozarek <karonkacper@gmail.com>
2020-04-16 16:03:32 +0200

    added payload to question model in db

--- WIET_sourcing/models/question.py
+++ WIET_sourcing/models/question.py
@@ -14,5 +14,6 @@ class Question(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     question_set_id = db.Column(db.Integer, db.ForeignKey('question_set.id'), nullable=False)
     type = db.Column(db.Enum(QuestionType), nullable=False)
+    payload = db.Column(db.JSON, nullable=False)
 
     question_set = db.relationship('QuestionSet', backref=db.backref('questions', lazy=True))

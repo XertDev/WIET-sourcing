8b90921cd5334876964ced786031d957cfbe2f59
maciektr <mtratnowiecki@gmail.com>
2020-04-19 13:29:26 +0200

    Fix fallback sqlalchemy db uri

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI',
-                                    'sqlite:////tmp/your_default.db')
+                                    'sqlite:///:memory:')

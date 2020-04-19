7c69196b0b38e31d5c47bd44910d8d8f82b35933
maciektr <mtratnowiecki@gmail.com>
2020-04-19 11:55:10 +0200

    Use silent mode for instance import

--- WIET_sourcing/__init__.py
+++ WIET_sourcing/__init__.py
@@ -12,7 +12,7 @@ def create_app(config=None):
 	app = Flask(__name__, instance_relative_config=True)
 
 	app.config.from_object('config')
-	app.config.from_pyfile('config.py')
+	app.config.from_pyfile('config.py', silent=True)
 
 	db.init_app(app)
 	migrate.init_app(app, db)

1957ba1943d2be70b97b12482e5f41c7376b0042
maciektr <mtratnowiecki@gmail.com>
2020-05-11 22:33:03 +0200

    Remove login template

--- WIET_sourcing/views.py
+++ WIET_sourcing/views.py
@@ -7,15 +7,10 @@ from WIET_sourcing.service.auth import get_logged_in_user
 def register_views(app):
     app.context_processor(base_context)
     app.add_url_rule('/', view_func=index)
-    app.add_url_rule('/login', view_func=login)
 
 
 def index():
     return render_template('index.html')
-
-
-def login():
-    return render_template('login.html')
 
 
 def base_context():

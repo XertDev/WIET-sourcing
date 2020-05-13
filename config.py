9e2f07a06a527c26f62a42a9ccf739ecd881b34b
maciektr <mtratnowiecki@gmail.com>
2020-05-13 15:04:20 +0200

    Fix typo

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI',
-                                    'sqlite:///' + os.path.join(basedir, 'WIET-sourcing/appdb.db'))
+                                    'sqlite:///' + os.path.join(basedir, 'WIET_sourcing/appdb.db'))
 
 KEY_SIGNING_SECRET = os.getenv("WIET_SIGNING_SECRET",
                                'WIET-SIGNING-SECRET')
